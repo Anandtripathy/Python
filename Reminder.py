cfedf26a0f12fef86919d51819112512de962d4b
Anand Tripathy <anandt.14@gmail.com>
2021-01-03 23:45:14 +0000

    Reminder

--- Reminder.py
+++
@@ -1,8 +0,0 @@
-import time
-print ("What Shall I remind you ?")
-text=str(input())
-print("In how many minutes?")
-local_time=float(input())
-local_time=local_time*60
-time.sleep(local_time)
-print(text)
